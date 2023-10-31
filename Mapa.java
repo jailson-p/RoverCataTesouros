67c99478a0b3fb260aca74a82cb346a51f278507
Jailson Pedro <jailson.p@aluno.ifsc.edu.br>
2023-10-31 11:20:47 -0300

    implementação da modelagem

---
+++ Mapa.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Mapa {
+
+    private  int largura ;
+    private  int altura ;
+
+    private  int totalDeTesouros;
+    private  ArrayList<Tesouro> tesouros;
+
+    public Mapa(int largura, int altura, int totalDeTesouros) {
+        this.largura = largura;
+        this.altura = altura;
+        this.totalDeTesouros = totalDeTesouros;
+        this.tesouros = new ArrayList<>();
+        this.posicionaTesouros();
+    }
+    public boolean posicionaTesouros(){
+        //TODO posicionarTesouros
+        return false;
+    }
+    public Tesouro coletarTesouro(int x,int y){
+        //TODO coletarTesouro
+        return null;
+    }
+    public int getLargura() {
+        return largura;
+    }
+    public int getAltura() {
+        return altura;
+    }
+    public int getTotalDeTesouros() {
+        return totalDeTesouros;
+    }
+}
---
+++ Tesouro.java
@@ -0,0 +1,23 @@
+public class Tesouro {
+    public static final int LARGURA = 10;
+    public static final int ALTURA  = 10;
+    private  int posicaoX;
+    private  int posicaoY;
+    private int valor;
+
+    public Tesouro(int posicaoX, int posicaoY, int pontos) {
+        this.posicaoX = posicaoX;
+        this.posicaoY = posicaoY;
+        this.valor = pontos;
+    }
+
+    public int getPosicaoX(){
+        return this.posicaoX;
+    }
+    public int getPosicaoY(){
+        return this.posicaoY;
+    }
+    public int getValor(){
+        return this.valor;
+    }
+}
